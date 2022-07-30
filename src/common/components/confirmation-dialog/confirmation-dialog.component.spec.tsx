e3bdc7169047d2748dd1c17ffea50fd9856b69c9
JorgeCh92 <jorgecalle.92@gmail.com>
2022-07-31 00:10:35 +0200

    Finish testing components

--- src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
+++ src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -20,11 +20,124 @@ describe('ConfirmationDialogComponent component specs', () => {
         //Act
         render(<ConfirmationDialogComponent {...props} />);
 
-        const buttonElement = screen.queryByRole('heading', {
+        const testDialog = screen.queryByRole('heading', {
             name: /test dialog/i,
         });
 
         //Assert
-        expect(buttonElement).toBe(null);
-    })
+        expect(testDialog).toBe(null);
+    });
+
+    it('should render pass when passing props required', () => {
+        //Arrange
+        const props = {
+            isOpen: true,
+            onAccept: jest.fn(),
+            onClose: jest.fn(),
+            title: 'Test dialog',
+            labels: {
+                closeButton: "Close Button",
+                acceptButton: "Accept Button",
+            },
+        };
+
+        //Act
+        render(<ConfirmationDialogComponent {...props} />);
+
+        const testDialog = screen.getByRole('heading', {
+            name: /Test dialog/i,
+        });
+
+        const buttonAcept= screen.getByRole('button', {
+            name: /accept/i,
+        });
+
+        const buttonClose= screen.getByRole('button', {
+            name: /close/i,
+        });
+
+        //Assert
+        expect(testDialog).toBeInTheDocument();
+        expect(buttonAcept).toBeInTheDocument();
+        expect(buttonClose).toBeInTheDocument();
+    });
+
+    it('should render pass when passing children', () => {
+        //Arrange
+        const props = {
+            isOpen: true,
+            onAccept: jest.fn(),
+            onClose: jest.fn(),
+            title: 'Test dialog',
+            labels: {
+                closeButton: "Close Button",
+                acceptButton: "Accept Button",
+            },
+        };
+
+        //Act
+        render(<ConfirmationDialogComponent {...props} >
+                <p>Children from dialog</p>
+            </ConfirmationDialogComponent>    
+        );
+
+        const testDialog = screen.getByText(
+            /Children from dialog/i
+        );
+
+        //Assert
+        expect(testDialog).toBeInTheDocument();
+    });
+
+    it('should render pass when click on accept button', () => {
+        //Arrange
+        const props = {
+            isOpen: true,
+            onAccept: jest.fn(),
+            onClose: jest.fn(),
+            title: 'Test dialog',
+            labels: {
+                closeButton: "Close Button",
+                acceptButton: "Accept Button",
+            },
+        };
+
+        //Act
+        render(<ConfirmationDialogComponent {...props} />);
+
+        const acceptButton = screen.getByRole('button', {
+            name: /accept/i,
+        });
+
+        fireEvent.click(acceptButton)
+
+        //Assert
+        expect(props.onAccept).toHaveBeenCalled();
+    });
+
+    it('should render pass when click on close button', () => {
+        //Arrange
+        const props = {
+            isOpen: true,
+            onAccept: jest.fn(),
+            onClose: jest.fn(),
+            title: 'Test dialog',
+            labels: {
+                closeButton: "Close Button",
+                acceptButton: "Accept Button",
+            },
+        };
+
+        //Act
+        render(<ConfirmationDialogComponent {...props} />);
+
+        const closeButton = screen.getByRole('button', {
+            name: /close/i,
+        });
+
+        fireEvent.click(closeButton)
+
+        //Assert
+        expect(props.onClose).toHaveBeenCalled();
+    });
 })
